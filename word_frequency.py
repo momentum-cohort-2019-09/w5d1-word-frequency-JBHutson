38a2e777483445db4251b9f771960faf6a0e461d
JBHutson <hutsonjb@gmail.com>
2019-10-07 10:10:26 -0400

    finished

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+import string
+import operator
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -6,8 +9,30 @@ STOP_WORDS = [
 
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+    word_count_dict = {}
+    with open(file) as opened_file:
+        line = opened_file.readline()
+        while line:
+            line = line.lower()
+            table = str.maketrans({key: None for key in string.punctuation})
+            line = line.translate(table)
+            split_line = line.split()
+            for word in split_line:
+                if word in STOP_WORDS:
+                    continue
+                if word in word_count_dict.keys():
+                    word_count_dict[word] += 1
+                elif word not in word_count_dict.keys():
+                    word_count_dict[word] = 1
+            line = opened_file.readline()
+    sorted_word_tup = sorted(word_count_dict.items(), key=operator.itemgetter(1), reverse=True)
+    for item in sorted_word_tup:
+        word = item[0]
+        ocur = item[1]
+        ast = ''
+        for _ in range(ocur):
+            ast = ast + '*'
+        print (f"{word} | {ocur} {ast}")
 
 
 if __name__ == "__main__":
